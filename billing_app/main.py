b8bc43844a75ff63b5dc50d196c235548aa4aa31
Anand Kumar <anandraz066@gmail.com>
2022-12-17 17:48:06 +0530

    initial commit

---
+++ billing_app/main.py
@@ -0,0 +1,8 @@
+# Install below package before starting 
+# pip install PyQt5
+# pip install pyqt5-tools==5.15.4.3.2
+
+"""
+Designer application path: C:\Users\Anand\AppData\Local\Programs\Python\Python38\Lib\site-packages\qt5_applications\Qt\bin\designer
+"""
+
